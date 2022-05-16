cbf8f5de5460f379141f09891ff843062091b62a
matthcol <matthias.colin@gmail.com>
2022-05-16 16:04:58 +0200

    proc with parameters (real)

--- movie_proc_fun_trig.sql
+++ movie_proc_fun_trig.sql
@@ -17,10 +17,35 @@ end;
 /
 
 -- count movies from one year
-create or replace function movie_count_by_year(p_year movies.year%type) return number is
+create or replace function movie_count_by_year(p_year number) return number is
     v_movie_count number;
 begin
     select count(*) into v_movie_count from movies where year = p_year;
     return v_movie_count;
 end;
 /
+
+create or replace procedure create_movie(
+    p_title in varchar2, 
+    p_year in number := NULL,
+    p_id out number
+) is
+begin
+    insert into movies (title, year) values (p_title, p_year);
+    -- select ISEQ$$_73704.currval into p_id from dual;
+    p_id := ISEQ$$_73704.currval;
+end;
+/
+
+-- not possible with Oracle 11 and before
+create or replace function f_create_movie(
+    p_title in varchar2, 
+    p_year in number := NULL
+) return  number is
+begin
+    insert into movies (title, year) values (p_title, p_year);
+    -- select ISEQ$$_73704.currval into p_id from dual;
+    return ISEQ$$_73704.currval;
+end;
+/
+
